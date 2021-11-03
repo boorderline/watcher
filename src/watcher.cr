3def6d8cf73534643f5e801efd39df8d1804bd07
Tudor <tudor@marghidanu.com>
2021-11-03 14:52:31 +0200

    Cleanup main body

--- src/watcher.cr
+++ src/watcher.cr
@@ -1,70 +1,40 @@
 require "log"
-require "http/server"
 require "option_parser"
+require "http/server"
 
-require "./config"
-require "./helm"
+require "./application"
 
 module Watcher
   VERSION = "0.1.0"
 
-  class Application
-    Log = ::Log.for(self)
-
-    getter config : Watcher::Config::App
-
-    def initialize(@config)
+  def self.run_workers(config_dir : String)
+    if config_dir.nil?
+      raise "Missing configuration directory!"
     end
 
-    def run
-      Log.info { "Starting watcher for: #{@config.name}" }
-      helm = Watcher::Helm::Client.new
-
-      loop do
-        begin
-          latest_entry = helm.get_latest_chart(
-            chart: @config.source.chart,
-            repo: @config.source.repository,
-            username: @config.source.repository_username,
-            password: @config.source.repository_password,
-          )
-
-          # Check if deployment exists
-          latest_release = helm.list_releases(@config.target.namespace)
-            .find { |r| r.name == @config.target.name }
-          latest_version = latest_release.nil? ? "" : latest_release.extract_chart_version(@config.source.chart)
-
-          if latest_version != latest_entry.version
-            Log.info { "New release for #{@config.target.name} at version #{latest_entry.version}" }
-
-            # TODO: Check additional rules. Probably we need to define them.
-            values = @config.target.values.nil? ? nil : YAML.dump(@config.target.values)
-
-            result = helm.deploy(
-              release: @config.target.name,
-              chart: @config.source.chart,
-              version: latest_entry.version,
-              repo: @config.source.repository,
-              username: @config.source.repository_username,
-              password: @config.source.repository_password,
-              namespace: @config.target.namespace,
-              create_namespace: @config.target.create_namespace,
-              values: values,
-            )
-
-            Log.info { "Successfully deployed #{@config.target.name} at revision #{result.version}" }
-          end
-
-          # TODO: Also check if the values have changed and trigger a new release if so ...
-        rescue ex
-          Log.error { ex.message }
-        end
-
-        interval = Time::Span.new(seconds: @config.scrape.interval)
-        Log.info { "Next iteration in #{interval}" }
-        sleep interval
-      end
+    if !Dir.exists?(config_dir.as(String))
+      raise "Directory #{config_dir} doesn't exist!"
     end
+
+    apps = Watcher::Config.load_from_dir(config_dir.as(String))
+    Log.info { "Loaded #{apps.size} configurations from #{config_dir}" }
+
+    if apps.empty?
+      raise "Did dot find any configurations. Exiting..."
+    end
+
+    apps.each { |c| spawn Watcher::Application.new(c).run }
+  end
+
+  def self.run_web
+    server = HTTP::Server.new do |context|
+      context.response.content_type = "text/plain"
+      context.response.print "Hello world!"
+    end
+
+    address = server.bind_tcp "0.0.0.0", 8080
+    Log.info { "Listening on http://#{address}" }
+    server.listen
   end
 end
 
@@ -84,32 +54,8 @@ begin
     parser.invalid_option { |flag| raise "Unkown flag: #{flag}" }
   end
 
-  if config_dir.nil?
-    raise "Missing configuration directory!"
-  end
-
-  if !Dir.exists?(config_dir.as(String))
-    raise "Directory #{config_dir} doesn't exist!"
-  end
-
-  apps = Watcher::Config.load_from_dir(config_dir.as(String))
-  Log.info { "Loaded #{apps.size} configurations from #{config_dir}" }
-
-  if apps.empty?
-    raise "Did dot find any configurations. Exiting..."
-  end
-
-  apps.each { |config| spawn Watcher::Application.new(config).run }
-
-  # ... Starting a webserver to display some info.
-  server = HTTP::Server.new do |context|
-    context.response.content_type = "text/plain"
-    context.response.print "Hello world!"
-  end
-
-  address = server.bind_tcp "0.0.0.0", 8080
-  puts "Listening on http://#{address}"
-  server.listen
+  Watcher.run_workers(config_dir)
+  Watcher.run_web
 rescue ex
   STDERR.puts "ERROR: #{ex.message}"
   exit(1)
