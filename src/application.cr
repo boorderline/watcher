c308153286e224f39daa67d3c96a4d7758de8553
Tudor <tudor@marghidanu.com>
2021-11-04 13:38:48 +0200

    Reacting if values changed

--- src/application.cr
+++ src/application.cr
@@ -27,14 +27,12 @@ module Watcher
           latest_release = @helm.list_releases(@config.target.namespace)
             .find { |r| r.name == @config.target.name }
           latest_version = latest_release.nil? ? "" : latest_release.extract_chart_version(@config.source.chart)
+          values = @config.target.values.nil? ? nil : YAML.dump(@config.target.values)
 
           # TODO: Check additional rules. Probably we need to define them.
 
           if latest_version != latest_chart.version
             Log.info { "New release for #{@config.target.name} at version #{latest_chart.version}" }
-
-            # TODO: Also check if the values have changed and trigger a new release if so ...
-            values = @config.target.values.nil? ? nil : YAML.dump(@config.target.values)
 
             result = @helm.deploy(
               release: @config.target.name,
@@ -49,13 +47,34 @@ module Watcher
             )
 
             Log.info { "Successfully deployed #{@config.target.name} at revision #{result.version}" }
+          else
+            release_values = @helm.get_revision_values(
+              release: @config.target.name,
+              namespace: @config.target.namespace,
+            )
+
+            if release_values != @config.target.values
+              Log.info { "Configuration changed for #{@config.target.name}, triggering re-deploy..." }
+
+              @helm.deploy(
+                release: @config.target.name,
+                chart: @config.source.chart,
+                version: latest_chart.version,
+                repo: @config.source.repository,
+                username: @config.source.repository_username,
+                password: @config.source.repository_password,
+                namespace: @config.target.namespace,
+                create_namespace: @config.target.create_namespace,
+                values: values,
+              )
+            end
           end
         rescue ex
           Log.error { ex.message }
         end
 
         interval = Time::Span.new(seconds: @config.scrape.interval)
-        Log.info { "Next iteration in #{interval}" }
+        Log.info { "Fisnihed checking '#{@config.name}'. Next iteration in #{interval}" }
         sleep(interval)
       end
     end
