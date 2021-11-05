472b5a4e62d3837d1175fccfa36f67e039265fab
Tudor <tudor@marghidanu.com>
2021-11-05 20:26:03 +0200

    Consolidating deployment rules

--- src/application.cr
+++ src/application.cr
@@ -27,12 +27,17 @@ module Watcher
       latest_version = latest_release.nil? ? "" : latest_release.extract_chart_version(@config.source.chart)
       values = @config.target.values.nil? ? nil : YAML.dump(@config.target.values)
 
+      release_values = @helm.get_revision_values(
+        release: @config.target.name,
+        namespace: @config.target.namespace,
+      )
+
       # TODO: Check additional rules. Probably we need to define them.
 
-      if latest_version != latest_chart.version
-        Log.info { "New release for #{@config.target.name} at version #{latest_chart.version}" }
+      if (latest_version != latest_chart.version) || (!@config.target.values.nil? && (release_values != @config.target.values))
+        Log.info { "Trigger new release for #{@config.name}" }
 
-        result = @helm.deploy(
+        @helm.deploy(
           release: @config.target.name,
           chart: @config.source.chart,
           version: latest_chart.version,
@@ -44,28 +49,7 @@ module Watcher
           values: values,
         )
 
-        Log.info { "Successfully deployed #{@config.target.name} at revision #{result.version}" }
-      else
-        release_values = @helm.get_revision_values(
-          release: @config.target.name,
-          namespace: @config.target.namespace,
-        )
-
-        if !@config.target.values.nil? && (release_values != @config.target.values)
-          Log.info { "Configuration changed for #{@config.target.name}, triggering re-deploy..." }
-
-          @helm.deploy(
-            release: @config.target.name,
-            chart: @config.source.chart,
-            version: latest_chart.version,
-            repo: @config.source.repository,
-            username: @config.source.repository_username,
-            password: @config.source.repository_password,
-            namespace: @config.target.namespace,
-            create_namespace: @config.target.create_namespace,
-            values: values,
-          )
-        end
+        Log.info { "Successfully  deployed a new release for #{@config.name}" }
       end
     rescue ex
       Log.error { ex.message }
